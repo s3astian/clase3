ca48a040c4a92ab8ebe7ed9afae5f73d17bc1401
jhoan diaz <jhoandiaz24@gmail.com>
2024-07-13 12:48:36 -0500

    ejerciosio3

--- variables-ejercisio1.py
+++ variables-ejercisio1.py
@@ -1,14 +1,31 @@
-nombre = "Tony Soprano"
-edad = 51
-print ("su nombre es " + nombre +  "su edad es "  ,edad  )
+# nombre = "Tony Soprano"
+# edad = 51
+# print ("su nombre es " + nombre +  "su edad es "  ,edad  )
 
-nombre_1 = "julia"
-apellido_1 = "Roberts" 
-nombrecompleto = (nombre_1+" "+apellido_1 )
-print(nombrecompleto)
+# nombre_1 = "julia"
+# apellido_1 = "Roberts" 
+# nombrecompleto = (nombre_1+" "+apellido_1 )
+# print(nombrecompleto)
 
-curso ="python" 
-print("esta tomando el curso de" + " " + curso ) 
+# curso ="python" 
+# # print("esta tomando el curso de" + " " + curso ) 
 
+# numero = 5
+# print (type (numero))
+
+# numero1 = 0.1
+# print (type (numero1))
+
+num_entero = 10
+print(type(num_entero))
+
+num_decimal = 0.10
+print(type(num_decimal))
+
+num1 = 7.5
+num2 = 2.5
+sum = num1+ num2
+print(sum)
+print(type(sum))
 
 
